d42d796cd08bf99b5b345bd0f2479919eabf15a3
AndrewKhans <47328437+AndrewKhans@users.noreply.github.com>
2019-03-21 19:28:43 -0400

    Edited README

--- src/main/java/andrewmogo/item_mandolin/Mandolin.java
+++ src/main/java/andrewmogo/item_mandolin/Mandolin.java
@@ -5,10 +5,7 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -19,32 +16,14 @@ import org.lwjgl.opengl.GL11;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class Mandolin extends ItemFood
+public class Mandolin extends ItemBow
 {
 
     public Mandolin() {
 
-        super(0, 0, false);
         this.setMaxStackSize(1);
-        this.setCreativeTab(CreativeTabs.FOOD);
-        this.setAlwaysEdible();
+        this.setCreativeTab(CreativeTabs.TOOLS);
+
     }
-
-    protected void onFoodEaten(ItemStack itemstack, World world, EntityPlayer player) {
-        player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1200, 0));
-//        player.inventory.addItemStackToInventory(new ItemStack(ItemListxlfoodmod.glass_mug));
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-        tooltip.add(TextFormatting.BLUE + "Speed I (1:00)");
-    }
-
-    @Override
-    public EnumAction getItemUseAction(ItemStack stack) {
-        return EnumAction.DRINK;
-    }
-
-
 
 }
